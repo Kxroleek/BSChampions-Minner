e1836174fd1f7ff785a132557985bbce40d7fb28
Kxroleek <82545925+Kxroleek@users.noreply.github.com>
2025-03-16 08:39:23 +0100

    Update BSChampions-Minner.user.js

--- BSChampions-Minner.user.js
+++ BSChampions-Minner.user.js
@@ -15,37 +15,59 @@
 // ==================== Begin BSChampions Minner Configuration ====================
 // This is the default configuration
 
+function load(key, def) {
+    let res = localStorage.getItem("autobsc-" + key)
+
+    if (res === null) {
+        store(key, def)
+        return def
+    } else {
+        return JSON.parse(res)
+    }
+}
+
+function store(key, val) {
+    localStorage.setItem("autobsc-"+key, JSON.stringify(val))
+}
+
+// ==================== Begin AutoBSC Configuration ====================
+// This is the default configuration
+// load(config_key, default_value)
+// Do not change the config key unless you know what you are doing
+
 // Auto send cheer, +5 points
-let cheerEnabled = true;
+let cheerEnabled = load("cheer", true);
 
 // Auto send poll (choosing MVP), always choose the first option, +100 points
-let pollEnabled = true;
+let pollEnabled = load("poll", true);
 
 // Auto send quiz, always choose correct option
-let quizEnabled = true;
+let quizEnabled = load("quiz", true);
 
 // Auto send match prediction
-// Note: It's recommended to submit your own prediction before matches start. If the semifinals or grand finals don't follow your previous predictions, you will have chance to submit a new prediction and you can let AutoBSC to do it for you.
-let matchPredictionEnabled = false;
+let matchPredictionEnabled = load("matchPrediction", false);
 
 // Team selection strategy
 // Can be 1 (select first team), 2 (select second team), rand (select random), maj (follow majority)
 // This setting will only be used if match prediction is enabled
-let matchPredictionStrategy = "maj"
+let matchPredictionStrategy = load("predictionStrategy", "maj")
 
 // Auto collect lootdrops (randomly appearing 10 point drops)
-let dropEnabled = true;
+let dropEnabled = load("drop", true);
+
+// Auto collect sliders
+let sliderEnabled = load("slider", true);
 
 // Log events (such as sending cheer) to the feed
-let feedLoggingEnabled = true;
+let feedLoggingEnabled = load("feedLogging", true);
 
 // Remove cheer graphics (improves performance? haven't tested but pretty sure it does)
-let lowDetail = false;
+let lowDetail = load("lowDetail", false);
 
 // Debug logging of websocket messages to console
 let debug = false;
 
-// ===================== End BSChampions Minner Configuration =====================
+// ===================== End AutoBSC Configuration =====================
 
 let feed;
 
@@ -57,39 +79,26 @@ function log(msg) {
     feed = document.getElementsByClassName("Feed__content")[0];
     if (!feed) {return}
   }
-
-  feed.children[feed.children.length - 2].insertAdjacentHTML("afterend", `<div data-v-10a27b37="" data-v-48743964="">
-  <div
-    data-v-69011ee0=""
-    data-v-10a27b37=""
-    class="Container"
-    style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px)"
-  >
-    <div data-v-7b4ba43f="" data-v-69011ee0="" class="BaseCard BaseCard--rmedium">
-      <div
-        data-v-69011ee0=""
-        data-v-7b4ba43f=""
-        class="ContentCard ContentCard--disabled ContentCard--inactive ContentCard--isFullWidth ContentCard--isCelebration"
-      >
-        <div data-v-69011ee0="" data-v-7b4ba43f="" class="ContentCard__celebration">
-          <div data-v-10a27b37="" data-v-7b4ba43f="" class="RewardCard">
-          <div data-v-69011ee0="" data-v-7b4ba43f="" class="ContentCard__celebration__background"></div>
-          <div data-v-69011ee0="" data-v-7b4ba43f="" class="ContentCard__celebration__bottomContainer"></div>
-            <div data-v-10a27b37="" class="RewardCard__rewardContainer" data-v-7b4ba43f="">
-              <div data-v-10a27b37="" class="RewardCard__infoContainer">
-                <div data-v-10a27b37="" class="RewardCard__textContainer" style="opacity: 1">
-                  <div data-v-10a27b37="" class="RewardCard__textContainer__title">${msg}</div>
+  
+  feed.children[feed.children.length - 2].insertAdjacentHTML("afterend", `<div data-v-3dcc93da="" data-v-8a7cf7d7="" class="Container" style="translate: none; rotate: none; scale: none; transform: translate(0px);">
+    <div data-v-de4b4abb="" data-v-3dcc93da="" class="BaseCard BaseCard--rmedium">
+        <div data-v-3dcc93da="" class="ContentCard ContentCard--disabled ContentCard--inactive ContentCard--isFullWidth ContentCard--isCelebration">
+            <div data-v-3dcc93da="" class="ContentCard__celebration">
+                <div data-v-3dcc93da="" class="ContentCard__celebration__background"></div>
+                <div data-v-3dcc93da="" class="ContentCard__celebration__bottomContainer"></div>
+                <div data-v-8a7cf7d7="" class="RewardCard">
+                    <div data-v-8a7cf7d7="" class="RewardCard__rewardContainer">
+                        <div data-v-8a7cf7d7="" class="RewardCard__infoContainer">
+                            <div data-v-8a7cf7d7="" class="RewardCard__textContainer">
+                                <div data-v-8a7cf7d7="" class="RewardCard__textContainer__title">${msg}</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
-`)
-
+</div>`)
 };
 
 function purge(elements) {
@@ -97,7 +106,7 @@ function purge(elements) {
         try {
             elem.remove()
         } catch (e) {
-            console.warn("[BSChampions Minner] Failed to remove element", elem, e)
+            console.warn("[AutoBSC] Failed to remove element", elem, e)
         }
     }
 }
@@ -117,6 +126,7 @@ function purge(elements) {
   let lastQuizId = "";
   let lastDropId = "";
   let lastMatchPredictionId = "";
+  let lastSliderId = "";
 
   const OriginalWebSocket = window.WebSocket;
 
@@ -149,7 +159,7 @@ function purge(elements) {
         if (debug) {
           const parsed = JSON.parse(data);
 
-          console.log("[BSChampions Minner] Sending message:", data, parsed);
+          console.log("[AutoBSC] Sending message:", data, parsed);
         }
         originalSend.call(this, data);
       };
@@ -161,7 +171,7 @@ function purge(elements) {
   function parse(data, ws) {
     const msg = JSON.parse(data)
     if (debug) {
-      console.log("[BSChampions Minner] Received message:", msg, data);
+      console.log("[AutoBSC] Received message:", msg, data);
     }
 
     msg.forEach(event => {
@@ -202,7 +212,7 @@ function purge(elements) {
                 que.getElementsByTagName("button")[0].click()
               }
             } catch (e) {
-              console.error("[BSChampions Minner]", e)
+              console.error("[AutoBSC]", e)
             }
           }, 3500);
           lastPollId = event.payload.typeId;
@@ -222,7 +232,7 @@ function purge(elements) {
 
                 que.getElementsByClassName("MultiChoiceQuestionCard__button")[event.payload.correctAnswer.alternative].click()
               } catch (e) {
-                console.error("[BSChampions Minner]", e)
+                console.error("[AutoBSC]", e)
               }
             }
           }, 3500);
@@ -264,7 +274,7 @@ function purge(elements) {
               try {
                 a.getElementsByTagName("button")[team].click()
               } catch (e) {
-                console.error("[BSChampions Minner]", e)
+                console.error("[AutoBSC]", e)
               }
             }
           }, 10000);
@@ -282,10 +292,30 @@ function purge(elements) {
               try {
                 drop.getElementsByClassName("RectangleButton")[0].click()
               } catch (e) {
-                console.error("[BSChampions Minner]", e)
+                console.error("[AutoBSC]", e)
               }
             }
             lastDropId = event.payload.typeId
+          }, 2000)
+        }
+      }
+
+      if (messageType === "slider" && sliderEnabled) {
+        if (event.payload.typeId !== lastSliderId) {
+          log("Collecting slider")
+
+          setTimeout(() => {
+            for (let drop of document.getElementsByClassName("SliderQuestionCard")) {
+              try {
+                let elem = drop.getElementsByTagName("input")[0]
+                elem.value = "100"
+                elem.dispatchEvent(new InputEvent("input"))
+                elem.dispatchEvent(new Event("change"))
+              } catch (e) {
+                console.error("[AutoBSC]", e)
+              }
+            }
+            lastSliderId = event.payload.typeId
           }, 2000)
         }
       }
@@ -295,7 +325,7 @@ function purge(elements) {
   function setupAutoBsc() {
     loaded = true;
 
-    console.log("[BSChampions Minner] BSChampions Minner loaded");
+    console.log("[AutoBSC] AutoBSC loaded");
 
     const interval = setInterval(() => {
       const div = document.getElementsByClassName("Feed__content")[0];
@@ -312,7 +342,7 @@ function purge(elements) {
 
     setInterval(() => {
       if (reconnectButtonContainer.style.display !== "none") {
-        console.log("[BSChampions Minner] Reconnecting");
+        console.log("[AutoBSC] Reconnecting");
         reconnectButton.click();
       }
     }, 1000);
@@ -342,7 +372,7 @@ function purge(elements) {
 <div id="autobsc-overlay" style="position: absolute; top: 20%; z-index: 99999999; background: antiquewhite">
 <details>
 <summary style="list-style: none;" id="autobsc-overlayheader" onclick="if (getAttribute('drag') === '') event.preventDefault()">
-  <div style="padding: 1rem;">BSChampions Minner</div>
+  <div style="padding: 1rem;">AutoBSC++</div>
 </summary>
 
 <div style="display: grid; justify-content: center; margin-bottom: .5rem;">
@@ -363,17 +393,19 @@ function purge(elements) {
 
   <div class="autobsc-config-container">Answer polls <input type="checkbox" id="autobsc-poll"></div>
 
-  <div class="autobsc-config-container">Answer Quiz <input type="checkbox" id="autobsc-quiz"></div>
+  <div class="autobsc-config-container">Answer quiz <input type="checkbox" id="autobsc-quiz"></div>
 
-  <div class="autobsc-config-container">Collect Lootdrop <input type="checkbox" id="autobsc-lootdrop"></div>
+  <div class="autobsc-config-container">Answer slider <input type="checkbox" id="autobsc-slider"></div>
+
+  <div class="autobsc-config-container">Collect lootdrop <input type="checkbox" id="autobsc-lootdrop"></div>
 
   <div class="autobsc-config-container">Autopredict <input type="checkbox" id="autobsc-predict"></div>
 
-  <div class="autobsc-config-container">Autopredict Strategy <select style="width: 3.825rem;" id="autobsc-predict-strat">
+  <div class="autobsc-config-container">Autopredict strategy <select style="width: 3.825rem;" id="autobsc-predict-strat">
   <option value="1">Blue</option>
   <option value="2">Red</option>
   <option value="rand">Random</option>
-  <option value="maj">Follow Majority</option>
+  <option value="maj">Follow majority</option>
 </select></div>
 
   <div class="autobsc-config-container">Feed logging <input type="checkbox" id="autobsc-feedlogging"></div>
@@ -391,6 +423,7 @@ function purge(elements) {
       cheer: document.getElementById("autobsc-cheer"),
       poll: document.getElementById("autobsc-poll"),
       quiz: document.getElementById("autobsc-quiz"),
+      slider: document.getElementById("autobsc-slider"),
       lootdrop: document.getElementById("autobsc-lootdrop"),
       predict: document.getElementById("autobsc-predict"),
       predictstrat: document.getElementById("autobsc-predict-strat"),
@@ -401,38 +434,52 @@ function purge(elements) {
     elems.cheer.checked = cheerEnabled
     elems.poll.checked = pollEnabled
     elems.quiz.checked = quizEnabled
+    elems.slider.checked = sliderEnabled
     elems.predict.checked = matchPredictionEnabled
     elems.lootdrop.checked = dropEnabled
     elems.feedlogging.checked = feedLoggingEnabled
 
     elems.predictstrat.value = matchPredictionStrategy
-    elems.lowdetail.value = lowDetail
+    elems.lowdetail.checked = lowDetail
 
     elems.cheer.onchange = function(e) {
       cheerEnabled = e.target.checked
+      store("cheer", cheerEnabled)
     }
     elems.poll.onchange = function(e) {
       pollEnabled = e.target.checked
+      store("poll", pollEnabled)
     }
     elems.quiz.onchange = function(e) {
       quizEnabled = e.target.checked
+      store("quiz", quizEnabled)
+    }
+
+    elems.slider.onchange = function(e) {
+      sliderEnabled = e.target.checked
+      store("slider", sliderEnabled)
     }
     elems.predict.onchange = function(e) {
       matchPredictionEnabled = e.target.checked
+      store("matchPrediction", matchPredictionEnabled)
     }
     elems.lootdrop.onchange = function(e) {
       dropEnabled = e.target.checked
+      store("drop", dropEnabled)
     }
     elems.feedlogging.onchange = function(e) {
       feedLoggingEnabled = e.target.checked
+      store("feedLogging", feedLoggingEnabled)
     }
 
     elems.predictstrat.onchange = function(e) {
       matchPredictionStrategy = e.target.value
+      store("predictionStrategy", matchPredictionStrategy)
     }
 
     elems.lowdetail.onchange = function(e) {
         lowDetail = e.target.checked
+        store("lowDetail", lowDetail)
         if (!lowDetail) {
             return
         }
@@ -445,47 +492,29 @@ function purge(elements) {
     matchpredred = document.getElementById("autobsc-pick-red")
   }
 
-  const loadedMessageHtml = `<div data-v-10a27b37="" data-v-48743964="">
-  <div
-    data-v-69011ee0=""
-    data-v-10a27b37=""
-    class="Container Container--extraTopMargin"
-    style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px)"
-  >
-    <div data-v-7b4ba43f="" data-v-69011ee0="" class="BaseCard BaseCard--rmedium">
-      <div
-        data-v-69011ee0=""
-        data-v-7b4ba43f=""
-        class="ContentCard ContentCard--disabled ContentCard--inactive ContentCard--isFullWidth ContentCard--isCelebration"
-      >
-        <div data-v-69011ee0="" data-v-7b4ba43f="" class="ContentCard__celebration">
-          <div data-v-69011ee0="" data-v-7b4ba43f="" class="ContentCard__celebration__background"></div>
-          <div data-v-69011ee0="" data-v-7b4ba43f="" class="ContentCard__celebration__bottomContainer"></div>
-          <div data-v-10a27b37="" data-v-7b4ba43f="" class="RewardCard">
-            <div data-v-10a27b37="" class="RewardCard__rewardContainer" data-v-7b4ba43f="">
-              <div data-v-10a27b37="" class="RewardCard__reward" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px)">
-                <picture data-v-3740ac92="" data-v-10a27b37="" class="cms-image cms-image--fullWidth cms-image--loaded cms-image--fullWidth"
-                  >
-                  <img
-                    data-v-3740ac92=""
-                    src="https://event.supercell.com/brawlstars/assets/rewards/images/emoji_starr.svg"
-                    class="cms-image cms-image--fullWidth cms-image--loaded cms-image--fullWidth"
-                /></picture>
-              </div>
-              <div data-v-10a27b37="" class="RewardCard__infoContainer">
-                <div data-v-10a27b37="" class="RewardCard__textContainer" style="opacity: 1">
-                  <div data-v-10a27b37="" class="RewardCard__textContainer__title">BSChampions Minner Loaded</div>
-                  <div data-v-10a27b37="" class="RewardCard__textContainer__subTitle">Made by Kxroleek</div>
+  const loadedMessageHtml = `<div data-v-3dcc93da="" data-v-8a7cf7d7="" class="Container Container--extraTopMargin" style="translate: none; rotate: none; scale: none; transform: translate(0px);">
+    <div data-v-de4b4abb="" data-v-3dcc93da="" class="BaseCard BaseCard--rmedium">
+        <div data-v-3dcc93da="" class="ContentCard ContentCard--disabled ContentCard--inactive ContentCard--isFullWidth ContentCard--isCelebration">
+            <div data-v-3dcc93da="" class="ContentCard__celebration">
+                <div data-v-3dcc93da="" class="ContentCard__celebration__background"></div>
+                <div data-v-3dcc93da="" class="ContentCard__celebration__bottomContainer"></div>
+                <div data-v-8a7cf7d7="" class="RewardCard">
+                    <div data-v-8a7cf7d7="" class="RewardCard__rewardContainer">
+                        <div data-v-8a7cf7d7="" class="RewardCard__reward">
+                            <picture data-v-afed0133="" data-v-8a7cf7d7="" class="cms-image cms-image--fullWidth cms-image--loaded cms-image--fullWidth"><img data-v-afed0133="" src="https://event.supercell.com/brawlstars/assets/rewards/images/emoji_starr.svg" class="cms-image cms-image--fullWidth cms-image--loaded cms-image--fullWidth"></picture>
+                        </div>
+                        <div data-v-8a7cf7d7="" class="RewardCard__infoContainer">
+                            <div data-v-8a7cf7d7="" class="RewardCard__textContainer">
+                                <div data-v-8a7cf7d7="" class="RewardCard__textContainer__title">AutoBSC++ loaded</div>
+                                <div data-v-8a7cf7d7="" class="RewardCard__textContainer__subTitle">made by laptopcat (based on AutoBSC by catme0w)</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
-`;
+</div>`;
 })();
 
 function dragElement(elmnt) {
